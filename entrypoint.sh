f79c35eeccd7b0e62547e790c0096e36ba41ecde
Kirill Losev <udarnik.kirill@gmail.com>
2026-09-17 22:00:13 +0300

    Changed entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn with 3 workers..."
-exec gunicorn hobza.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn swipe_nails.wsgi:application --bind 0.0.0.0:8000 --workers 3
