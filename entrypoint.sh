9f5c645d4e71e3f4e3e348f6ad28d20d6025fd06
Kirill Losev <udarnik.kirill@gmail.com>
2026-09-17 21:13:49 +0300

    Added Dockerfile and docker settings

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn with 3 workers..."
+exec gunicorn hobza.wsgi:application --bind 0.0.0.0:8000 --workers 3
